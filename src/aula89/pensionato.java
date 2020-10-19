5a44b4dd8bc44d68f33247cf23600cb9659b4cd6
Caio Pereira <ch.caio@hotmail.com>
2020-10-19 10:11:23 -0300

    initial commit

---
+++ src/aula89/pensionato.java
@@ -0,0 +1,55 @@
+/*
+Fazer um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o número de 
+estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes. Para cada 
+registro de aluguel, informar o nome e email do estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha
+que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupações do 
+pensionato, por ordem de quarto, conforme exemplo. */
+package aula89;
+import java.util.Locale;
+import java.util.Scanner;
+import rooms.roomsRent;
+
+public class pensionato {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Quantidade alunos");
+		int n = sc.nextInt();
+		
+		roomsRent [] vect = new roomsRent[9];
+		
+		for (int i=0; i<n; i++) {
+			sc.nextLine();
+			System.out.print("nome aluno: ");
+			String name = sc.nextLine();
+			
+			System.out.print("email aluno: ");
+			String email = sc.nextLine();
+			
+			System.out.print("quarto aluno: ");
+			int room = sc.nextInt();
+		
+			vect [room] = new roomsRent (name, email, room);
+		}
+		
+		for (int i=0; i<vect.length; i++) {
+		if (vect[i] != null) {
+			System.out.println(vect[i]);
+		}
+		}
+		sc.close();
+
+	}} // class & main
+
+
+
+
+
+
+
+
+
+
---
+++ src/rooms/roomsRent.java
@@ -0,0 +1,36 @@
+package rooms;
+
+public class roomsRent {
+	
+	private String name, email;
+	private int room;
+	
+	public roomsRent(String name, String email, int room) {
+		this.name = name;
+		this.email = email;
+		this.room = room;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getRoom() {
+		return room;
+	}
+	public void setRoom(int room) {
+		this.room = room;
+	}
+
+	public String toString () {
+		return "nome: " +name + ", e-mail: " + email + ", quarto: " + room;
+	}
+	
+}
